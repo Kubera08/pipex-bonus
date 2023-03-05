16a160339a34066060102f606dc5be0449472a6e
Arnaud BEAUDUIN <abeaudui@e2r10p7.clusters.42paris.fr>
2023-03-05 13:43:54 +0100

    pushbonus

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: abeaudui <abeaudui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:56:33 by abeaudui          #+#    #+#             */
-/*   Updated: 2023/03/05 13:40:41 by abeaudui         ###   ########.fr       */
+/*   Updated: 2023/03/05 13:43:44 by abeaudui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,47 +39,7 @@
 // le fils sera en mesure de lire depuis pipefd[0] les informations fournies par son père dans pipefd[1] et inversement
 
 #include "pipex.h"
-/*
-int main(int ac, char **av, char **env)
-{
-	t_pipex pipex;
-	
-	pipex.cmd1 = av[2];
-	pipex.cmd2 = av[3];
-	pipex.path_pos = path_pos(env);
-	pipex.envVec = ft_split(pipex.path_pos, ':');
-	pipex.argOptions1 = ft_split(pipex.cmd1, ' ');
-	pipex.argOptions2 = ft_split(pipex.cmd2, ' ');
-	pipex.infile = open(av[1], O_RDONLY);
-	pipex.outfile = open(av[ac -1], O_RDWR, O_CREAT);
-	if (pipex.infile != -1 && pipex.outfile != -1 && ac != 5)
-		return(3);
-	if (pipe(pipex.fd) == -1) // création pipe
-			return(1);
-	pipex.pid1 = fork();
-	if (pipex.pid1 < 0)
-		return(2);
-	if (pipex.pid1 == 0) // enfant 1 -> process pour notre deuxieme commande. Devient un parent quand on fork une deuxieme fois
-		second_child(pipex);
-	pipex.pid2 = fork();
-	if (pipex.pid2 == 0) // enfant 2 -> process de notre premiere commande. Lorsqu'on fork une deuxieme fois, il y a au total trois process car "on fork le fait de fork"
-		first_child(pipex);
-	else // père
-			daddy(pipex);
-	ft_free(pipex);
-	return(0);
-}
 
-
-void daddy(t_pipex pipex)
-{
-			close(pipex.fd[0]);
-			close(pipex.fd[1]);
-			waitpid(pipex.pid1, NULL, 0);
-			waitpid(pipex.pid2, NULL, 0);
-}
-
-*/
 
 
 // BONUS PART TEST
